d5760a6bade405b60cbf6650023c2f8c8d673804
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-05-22 23:33:41 -0300

    Head config AMP

--- www/themes/wordpress/header.php
+++ www/themes/wordpress/header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<script async src="https://cdn.ampproject.org/v0.js"></script>
+	<link rel="canonical" href="<?php echo get_home_url(); ?>">
 	<meta name="Description" content="<?php get_bloginfo( 'description', 'display' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
@@ -24,8 +25,7 @@
 	<meta name="theme-color" content="#ffa500">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
 
-    <link rel="profile" href="//gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
 	<?php wp_head(); ?>
 </head>
