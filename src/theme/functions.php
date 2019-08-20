596cfa8aae2a1d742e069b931808a76faecf989b
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-08-20 00:10:30 -0300

    remove jquery

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -123,3 +123,5 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+if ( !is_admin() ) wp_deregister_script('jquery');
