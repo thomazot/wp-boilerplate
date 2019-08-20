8c2439f9fcb16e06eceebf39fd5513480b1df1d0
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-08-20 13:05:41 -0300

    Remove emoji

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -131,3 +131,10 @@ function my_jquery_enqueue() {
 }
 
 add_action( 'wp_enqueue_scripts', 'my_jquery_enqueue' );
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
